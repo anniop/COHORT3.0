58aade5d8630aa85a75c26dfa09d97b88c037102
anniop <b.c114303@gmail.com>
2024-09-24 09:44:22 +0530

    COHORT 3.0

---
+++ week2/promises/class2.js
@@ -0,0 +1,25 @@
+class rectangle {
+    constructor(width,height, color) {
+        this.width = width;
+        this.height = height;
+        this.color = color;
+
+    }
+    area() {
+        const area = this.width * this.height;
+        return area;
+    }
+
+    paint() {
+        console.log("The Color is :");
+        console.log(this.color);
+    }
+}
+
+const rect = new rectangle(1,2,"Red");
+const area = rect.area();
+const paint = rect.paint();
+
+console.log(area);
+console.log(paint);
+
