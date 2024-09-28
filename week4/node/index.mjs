0400754fcb0d464e0793ae3d3f2e8d94e9e06ced
anniop <b.c114303@gmail.com>
2024-09-28 21:52:33 +0530

    COHORT 3.0

---
+++ week4/node/index.mjs
@@ -0,0 +1,7 @@
+import chalk  from "chalk"; // modern import syntax
+
+console.log(chalk.blue("Jay Ganesh"));
+console.log(chalk.red.bold("Jay Shri Krishna"));
+console.log(chalk.greenBright.underline("Har Har Mahadev"));
+
+// read more about chalk package at :- https://www.npmjs.com/package/chalk
