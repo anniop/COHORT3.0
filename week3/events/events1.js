eb5edade254b853f5cf9360a10b7c5972b1bcaf5
anniop <b.c114303@gmail.com>
2024-10-03 21:51:30 +0530

    COHORT 3.0

---
+++ week3/events/events1.js
@@ -0,0 +1,14 @@
+let btn1 = document.querySelector("#button1");
+
+btn1.onclick = () => {
+  console.log("button1 was clicked");
+  let a = 25;
+  a++;
+  console.log(a);
+  alert("Ganpati Bappa Morya!!!");
+}
+
+let div1 = document.querySelector("#div1");
+div1.onmouseover = () => {
+  console.log("Har Har Mahadev");
+}
