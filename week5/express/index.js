c411d734dec16aa7d9cfbfca6c9d24f4c4819c4c
anniop <b.c114303@gmail.com>
2024-10-04 09:02:53 +0530

    COHORT 3.0

---
+++ week5/express/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+
+app.get("/add", function (req, res) {
+  let a = req.query.a;
+  let b = req.query.b;
+
+  res.json({
+    ans: a + b
+  })
+});
+
+app.get("/substract", function (req, res) {
+  let a = req.query.a;
+  let b = req.query.b;
+
+  res.json({
+    ans: a - b
+  })
+});
+
+app.get("/multiply", function (req, res) {
+  let a = req.query.a;
+  let b = req.query.b;
+
+  res.json({
+    ans: a * b
+  })
+});
+
+app.get("/division", function (req, res) {
+  let a = req.query.a;
+  let b = req.query.b;
+
+  res.json({
+    ans: a / b
+  })
+});
+
+
+app.listen(3000);
