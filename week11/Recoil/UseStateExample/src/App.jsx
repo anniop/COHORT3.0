46a8321e6d53ec0d33d414642cb7cc74dd6b13bd
anniop <b.c114303@gmail.com>
2024-11-17 23:28:20 +0530

    COHORT 3.0

---
+++ week11/Recoil/UseStateExample/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Counter />
+    </>
+  )
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  return <div>
+    {count}
+    <Increase setCount={setCount} />
+    <Decrease setCount={setCount} />
+  </div>
+}
+
+function Decrease({ setCount }) {
+
+  function decrease() {
+    setCount(c => c - 1);
+  }
+
+  return <div>
+    <button onClick={decrease}>Decrease</button>
+  </div>
+}
+
+function Increase({ setCount }) {
+
+  function increase() {
+    setCount(c => c + 1);
+  }
+
+  return <div>
+    <button onClick={increase}>Increase</button>
+  </div>
+}
+
+export default App
